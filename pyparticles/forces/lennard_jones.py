df027def4ca7f27d794ec8b57bb1b532bec78b47
simon-r <simone.rva@gmail.com>
2012-10-23 22:18:22 +0200

    working in lenard jones

--- pyparticles/forces/lennard_jones.py
+++ pyparticles/forces/lennard_jones.py
@@ -21,10 +21,53 @@ import scipy.spatial.distance as dist
 import pyparticles.forces.force as fr
 
 class LenardJones( fr.Force ) :
-    def __init__( self , size , dim=3 , m=None , Consts=1.0 ):
+    def __init__( self , size , dim=3 , m=None , Consts=( 1.0 , 1.0 ) ):
         self.__dim = dim
         self.__size = size
-        self.__E = Consts
+        self.__E = Consts[0]
+        self.__O = Consts[1]
+
+        self.__M = np.zeros( ( size , 1 ) )
+        
+        self.__pF = np.zeros(( (size**2-size)/2 ))
+        
+        if m != None :
+            selt.set_masses( m )
+        
+        
+    def set_masses( self , m ):
+       """
+       Set the masses used for computing the forces.
+       """
+       self.__M[:] = m
+       
+    
+    def update_force( self , p_set ):
+        """
+        Compute the force of the current status of the system and return the accelerations of every particle in a *size by dim* array
+        """
+        
+        r =  dist.pdist( p_set.X , 'euclidean' )
+        
+        self.__pF[:] = 4.0 * self.__E * ( 12.0 * self.__O**12.0 / r**13.0 - 6.0 * self.__O**6.0 / r**7.0  )
         
         
         
+        return self.__A
+    
+    def getA(self):
+        """
+        Return the currents accelerations of the particles
+        """
+        return self.__A
+    
+    A = property( getA , doc="Return the currents accelerations of the particles (getter only)")
+
+
+    def getF(self):
+        """
+        Return the currents forces on the particles
+        """
+        return self.__A * self.__M[:,0]
+    
+    F = property( getF , doc="Return the currents forces on the particles (getter only)")
