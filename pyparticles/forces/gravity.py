68b13789f35028068e2455e3bf8db424956a7e10
simon-r <simone.rva@gmail.com>
2012-12-10 10:51:26 +0100

    Added gravity OCL

--- pyparticles/forces/gravity.py
+++ pyparticles/forces/gravity.py
@@ -18,6 +18,15 @@ import numpy as np
 import scipy.spatial.distance as dist
 import pyparticles.forces.force as fr
 
+import pyparticles.pset.opencl_context as occ 
+
+try:
+    import pyopencl as cl
+    import pyopencl.array as cla
+except:
+    ___foo = 0
+
+
 class Gravity( fr.Force ) :
     r"""
     Compute the gravitational force between the particles
@@ -114,7 +123,7 @@ class GravityOCL( fr.Force ) :
     :param    m:           a vector containig the masses
     :param    Const:       the gravitational constant
     """
-    def __init__(self , size , dim=3 , m=None , Consts=1.0 ):
+    def __init__(self , size , dim=3 , m=None , Consts=1.0 , ocl_context=None ):
         
         self.__dim = dim
         self.__size = size
@@ -129,6 +138,52 @@ class GravityOCL( fr.Force ) :
             self.set_masses( m )
         
         
+    def __init_prog_cl(self):
+        self.__gravity_prg = """
+        __kernel void drag(__global const float *X , 
+                           __global const float *M ,
+                                          float  G , 
+                           __global       float *A )
+        {
+            int i  = get_global_id(0) ;
+            int sz = get_global_size(0) ;
+            int n ;
+            float dist , f ;
+            
+            float4 at , u ;
+            
+            at.x = 0.0 ;
+            at.y = 0.0 ;
+            at.z = 0.0 ;
+            at.w = 0.0 ;
+            
+            u.w = 0.0 ;
+            
+            for ( n = 0 ; n < sz ; n++ ) 
+            {
+                if ( n == i ) continue ;
+                
+                u.x = X[3*i] - X[3*n]
+                u.y = X[3*i+1] - X[3*n+1]
+                u.z = X[3*i+2] - X[3*n+2]
+                
+                dist = length( u ) ;
+                
+                f = G * M[n] / pown( dist , 3 )
+                
+                at.x = at.x + f * u.x
+                at.y = at.x + f * u.y
+                at.z = at.x + f * u.z
+            } 
+            
+            A[3*i] = at.x
+            A[3*i+1] = at.y
+            A[3*i+2] = at.w
+        }
+        """
+        
+        self.__cl_program = cl.Program( self.__occ.CL_context , self.__gravity_prg ).build()    
+    
     
     def set_masses( self , m ):
         """
