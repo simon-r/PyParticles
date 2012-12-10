b1149266866d2918e88721d99912155770b80e70
simon-r <simone.rva@gmail.com>
2012-12-10 22:03:24 +0100

    Added insert array by name and edit docs

--- pyparticles/pset/opencl_context.py
+++ pyparticles/pset/opencl_context.py
@@ -30,9 +30,40 @@ OCLC_A = np.uint8( 0b00100000 )
 OCLC_M = np.uint8( 0b00010000 )
 
 class OpenCLcontext( object ):
+    """
+    Create a working context for PyOpenCL.
+        This class build a single opencl context and a command queue, that should be shared between the different method or algorithms. 
+        We can also use this class for minimizing  the data transfer between the main RAM and the GPU RAM or for sharing some TMP array.
+        
+    The Array used in this class are the ones defined in the pyopencl package: http://documen.tician.de/pyopencl/array.html#the-array-class 
+        
+        Constructor
+        
+        :param size: the size of the particles set
+        :param dim: The dimension of the system
+        :param mask: ( OCLC_X | OCLC_V | OCLC_A | OCLC_M ) setup the indicated arrays 
+            *. OCLC_X: Position array
+            *. OCLC_V: Velocity array
+            *. OCLC_A: Acceleration array
+            *. OCLC_M: Mass array
+        :param dtype: the floating point type (at the moment I support only np.float32 !! )
+        
+        Example: ::
+            
+            # Share the same context between gravity and the Euler Solver
+            
+            occx = occ.OpenCLcontext(  pset.size , pset.dim , (occ.OCLC_X|occ.OCLC_V|occ.OCLC_A|occ.OCLC_M) )
+            grav = gr.GravityOCL( pset.size , Consts=G , ocl_context=occx  )
+            solver = els.EulerSolverOCL( grav , pset , dt , ocl_context=occx )
+    """
     def __init__( self , size , dim , mask=( OCLC_X | OCLC_V | OCLC_A | OCLC_M ) , dtype=np.float32 ):
         
         self.__dtype = dtype 
+        
+        self.__size = size
+        self.__dim = dim  
+        
+        self.__opt_arrays = dict()
                 
         self.__cl_context = cl.create_some_context()
         self.__cl_queue = cl.CommandQueue(self.__cl_context, properties=cl.command_queue_properties.PROFILING_ENABLE )
@@ -66,6 +97,30 @@ class OpenCLcontext( object ):
         return self.__dtype
     
     dtype = property( get_dtype , doc="return the dtype of the context" )
+    
+    
+    def add_array_by_name( self , key , size=None , dim=None ):
+        """
+        Add a new array by name.
+        
+        :param key: The key (or name) of the new array
+        :param size: (Default val) The size of the new array, by default it uses the current size
+        :param dim: (Default val) The dim of the new array, by default it uses the current dim 
+        """
+        if size == None :
+            size = self.__size
+            
+        if dim == None :
+            dim == self.__dim
+        
+        self.__opt_arrays[key] = cla.Array( self.__cl_queue , ( size , 1 ) , self.dtype )
+    
+        
+    def get_by_name( self , key ):
+        """
+        Return the array named key
+        """
+        return self.__opt_arrays[key]
     
     
     def get_CL_context(self):
