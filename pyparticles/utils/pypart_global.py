54239de80e52fbbf1d2564ab003e1fa0f2e66240
Simone <simone.riva@bluewin.ch>
2024-02-04 18:04:23 +0100

    Update version to 0.4.0

--- pyparticles/utils/pypart_global.py
+++ pyparticles/utils/pypart_global.py
@@ -21,8 +21,8 @@ import zlib
 ###########################
 # Current version
 v_major    = 0
-v_minor    = 3
-v_revision = 6
+v_minor    = 4
+v_revision = 0
 ###########################
 
 def py_particle_version( r='s' ):
